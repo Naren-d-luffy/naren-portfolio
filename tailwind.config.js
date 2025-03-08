6ba7517d1a8119b077ed14b20c8e4f8941b61def
Naren <deadlyassassin2003@gmail.com>
2025-03-08 18:32:20 +0530

    completing Hero section

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+            openSans: ["var(--font-open-sans)", "sans-serif"],
+        },
+      },
+    },
+    plugins: [],
+  };
+  
