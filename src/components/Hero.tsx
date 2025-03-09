32ab06c323161b555e04d80f9f78868d7d9fd67b
Naren <deadlyassassin2003@gmail.com>
2025-03-09 11:41:28 +0530

    completing About section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import profile from "../../public/Adobe Express 1.png";
 
 export default function Hero() {
   return (
-    <div
+    <section
       id="hero"
       className="w-full h-screen flex flex-row lg:p-20 items-center justify-center text-center relative"
     >
@@ -12,19 +12,16 @@ export default function Hero() {
         <h1 className="text-7xl font-bold font-openSans mt-4 text-gray-950">
           I&apos;m Ram <span className="text-purple-800">Naren</span>
         </h1>
-        <p className="text-md mt-3 mb-6 mx-auto lg:pr-25">
+        <p className="text-md mt-3 mb-6 text-justify w-3/4">
           A passionate Full Stack Developer specializing in MERN stack, cloud
           technologies, and scalable web applications.
         </p>
         <a
-          href="https://personal-certificate.s3.eu-north-1.amazonaws.com/RamNaren's+CV.pdf"
-          download="RamNaren_CV.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
+        href="#contact"
           className="px-6 py-2 bg-gray-950 text-white rounded-lg relative overflow-hidden 
              hover:bg-purple-700 animated-ring"
         >
-          Preview CV
+          Contact Me
         </a>
       </div>
       <div data-aos="fade-up" className="relative w-1/2 flex justify-center">
@@ -37,6 +34,6 @@ export default function Hero() {
           className="rounded-full"
         />
       </div>
-    </div>
+    </section>
   );
 }
