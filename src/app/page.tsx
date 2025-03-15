8ca107243e9520709589fceed32844cbf48659d8
Naren <deadlyassassin2003@gmail.com>
2025-03-15 07:55:46 +0530

    Refactoring Contact me Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import EducationExperience from "../components/EducationExperience";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import Certifications from "../components/Certifications";
-// import Contact from "../components/Contact";
+import Contact from "../components/Contact";
 
 export default function Home() {
   return (
@@ -16,7 +16,7 @@ export default function Home() {
       <Skills />
       <Projects />
       <Certifications />
-      {/* <Contact /> */}
+      <Contact />
     </>
   );
 }
