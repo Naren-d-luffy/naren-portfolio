e679cbe4f94052551658d8055a313243b4d77919
Naren <deadlyassassin2003@gmail.com>
2025-03-24 18:37:42 +0530

    Making animation smooth

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="py-4 px-20 bg-violet-200 shadow-lg shadow-violet-400 fixed w-full top-0 z-50">
+    <header className="py-4 px-20 bg-violet-200 shadow-lg shadow-violet-400 fixed w-full top-0 z-50" data-aos="fade-down">
       <nav className="container mx-auto flex justify-between items-center px-4">
         <h1 className="text-xl font-bold text-violet-950">RamNaren&apos;s Portfolio</h1>
         <ul className="flex gap-6">
